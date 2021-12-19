de0e7df23c461162ce3bd61a3c39e7e1613296b2
acgist <289547414@qq.com>
2021-12-20 07:21:24 +0800

    [*] 配置统一

--- gateway/src/main/java/com/acgist/main/GatewayApplication.java
+++ gateway/src/main/java/com/acgist/main/GatewayApplication.java
@@ -1,11 +1,14 @@
 package com.acgist.main;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.config.web.servlet.oauth2.resourceserver.OAuth2ResourceServerSecurityMarker;
 
 @ComponentScan("com.acgist.gateway")
+@DubboComponentScan("com.acgist.gateway")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
