86177e32c24d8565d53abdfa7eeb5fea1a5b29e7
acgist <289547414@qq.com>
2022-03-05 15:23:32 +0800

    [+] 配置优化

--- docs/pack/bin/startup.sh
+++ docs/pack/bin/startup.sh
@@ -2,11 +2,12 @@
 
 bin_abs_path=$(readlink -f $(dirname $0))
 base=${bin_abs_path%/*}
+
 cd $base
 
 JAVA_OPTS_MEM="-server -Xms${system.maven.xms} -Xmx${system.maven.xmx}"
-JAVA_OPTS_EXT="-D file.encoding=${system.maven.encoding}"
-JAVA_OPTS_APP="-D spring.profiles.active=${profile}"
+JAVA_OPTS_EXT="-Dfile.encoding=${system.maven.encoding}"
+JAVA_OPTS_APP="-Dspring.profiles.active=${profile}"
 JAVA_OPTS="$JAVA_OPTS_MEM $JAVA_OPTS_EXT $JAVA_OPTS_APP ${system.maven.jvm}"
 
-java $JAVA_OPTS -jar $base/${project.artifactId}-${project.version}.jar
+java $JAVA_OPTS -jar $base/lib/${project.artifactId}-${project.version}.jar
